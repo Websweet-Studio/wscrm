197ba00391b4f574336b6668825256c0a822b09b
aadiityaak <aadiityaak@gmail.com>
2026-08-03 21:40:55 +0700

    feat(User): update isAdmin check to include employee users

--- app/Models/User.php
+++ app/Models/User.php
@@ -95,7 +95,7 @@ class User extends Authenticatable
      */
     public function isAdmin(): bool
     {
-        return in_array($this->role, ['admin', 'super_admin']);
+        return in_array($this->role, ['admin', 'super_admin']) || $this->isEmployee();
     }
 
     /**
